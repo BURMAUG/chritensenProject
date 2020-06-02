7d1709554b3127750004fe6f9252254a7c7d3f6e
Deejai <burmaud1@gmail.com>
2020-06-02 15:11:09 -0400

    made an extra folder for js files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//getting the image of the professor for the home page
+const professorImage = document.getElementById('prof-image');
+professorImage.style.height = '700px';
+professorImage.style.width = '650px';
+professorImage.style.borderRadius = '150px';
+
+
---
+++ js/classes.js
@@ -0,0 +1,13 @@
+//getting the div and setting the templete right and left
+const class_template_image = document.getElementById('class_template_image');
+class_template_image.style.paddingLeft = '150px';
+class_template_image.style.paddingRight = '150px';
+
+//get the images and reduce the borders
+const class_images = document.getElementsByTagName('img');
+//looping through al the images templates to adjust as desired
+for(let i = 0; i < class_images.length; i++){
+    class_images[i].style.borderRadius = '15px';
+    class_images[i].style.height = '400px';
+}
+
