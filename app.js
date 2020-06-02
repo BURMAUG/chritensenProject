cf4750b836203d175609618317855d9eb94e2598
Deejai <burmaud1@gmail.com>
2020-06-02 06:37:46 -0400

    added javascript

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ professorImage.style.width = '650px';
 professorImage.style.borderRadius = '150px';
 
 
+// style="justify-content: center; padding-left: 200px;"
+//I think i have to get the div and then try to center 
+//the images
+const class_template_image = document.getElementsByClassName('class_template_image');
+class_template_image.style.paddingLeft ='200px'; //something is wrong here => uncaught type error 'paddingLeft not defined'
+
