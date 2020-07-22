e3bd78f4e20e4500230223a9277fb5545dc0a317
Deejai <burmaud1@gmail.com>
2020-07-21 22:43:32 -0400

    more styling

--- js/precalculus.js
+++ js/precalculus.js
@@ -12,12 +12,13 @@ if(year.value === '2020'){
     year.value = option.value
 };
 
+// this is for the semester
 option = document.createElement('option');
 option.value = 'semester-select';
 option.hidden = true;
 option.textContent = 'Select a semester';
 semester.appendChild(option);
-
+//check if the vcalue is spring
 if(semester.value.toLowerCase() === 'spring'){
-    semester.value = option.value;
+    semester.value = option.value; //and make the value whatever options value is.
 }
